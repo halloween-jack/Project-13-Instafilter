2588c9184ca4098b4d5eead3a4109bced847d09b
halloween-jack <halloween.jack.dev@gmail.com>
2017-10-17 20:22:37 +0900

    Designing the interface

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: Properties
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Actions
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    @IBAction func save(_ sender: UIButton) {
+    }
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
 }
 
